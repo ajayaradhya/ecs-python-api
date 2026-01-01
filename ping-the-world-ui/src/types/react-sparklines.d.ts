894b638829de60c96509f00ad4e8ea1d35824a4c
Ajay B L <be.el.ajay@gmail.com>
2026-01-01 23:22:33 +0530

    Fix added to ract sparklines

---
+++ ping-the-world-ui/src/types/react-sparklines.d.ts
@@ -0,0 +1,8 @@
+declare module "react-sparklines" {
+  export const Sparklines: any;
+  export const SparklinesLine: any;
+  export const SparklinesBars: any;
+  export const SparklinesSpots: any;
+  export const SparklinesCurve: any;
+  export const SparklinesReferenceLine: any;
+}
